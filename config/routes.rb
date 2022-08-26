a7247871ee1dd9c403294f9f5aef2028980c86a1
大山一毅 <kazuki.oyama@tunagate.com>
2022-08-27 02:41:26 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'home#index'
+  root 'maps#index'
   # get '*path', to: 'home#index'
 
   get 'maps' => 'maps#index'
